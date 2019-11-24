73257eeac83c8704f18cecc708c7d5af6254f177
Roland Yeghiazaryan <roland.yeghiazaryan@gmail.com>
2019-11-24 18:12:21 +0400

    Update version name

--- lib/utils/devfest.dart
+++ lib/utils/devfest.dart
@@ -3,7 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Devfest {
   static const String app_name = "Devfest";
-  static const String app_version = "Version 1.0.4";
+  static const String app_version = "Version 1.0.0";
   static const int app_version_code = 1;
   static const String app_color = "#ffd7167";
   static Color primaryAppColor = Colors.white;
