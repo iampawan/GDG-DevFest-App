1bd1f6fb54a402eb04345688bba58b7d24076e36
Roland Yeghiazaryan <roland.yeghiazaryan@gmail.com>
2019-11-25 00:26:34 +0400

    Add "Become sponsor" button to sponsors list

--- lib/sponsors/sponsor_page.dart
+++ lib/sponsors/sponsor_page.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_devfest/home/home_widgets/home_front.dart';
 import 'package:flutter_devfest/universal/dev_scaffold.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SponsorPage extends StatelessWidget {
   static const String routeName = "/sponsor";
@@ -13,7 +16,18 @@ class SponsorPage extends StatelessWidget {
         children: <Widget>[
           SponsorImage(
             imgUrl: "https://lh3.googleusercontent.com/NGPrjka2ai0w7sfhxkxCwtOSh2wVyEZMdtrVxI4vrA22ebA_fcyl9PSvhTaCYXSWh0A68ZhvXhVZ4U-Nnp3v9IfoXg5o5H1tjjK97cs",
-          )
+          ),
+          ActionCard(
+            icon: FontAwesomeIcons.handshake,
+            color: Colors.purple,
+            title: "Become a sponsor for 2020 GDG events",
+            onPressed: () async {
+              var emailUrl =
+              '''mailto:gdg.yerevan@gmail.com?subject=Request for Sponsorship 2020''';
+              var out = Uri.encodeFull(emailUrl);
+              await _launchURL(out);
+            },
+          ),
 //          SizedBox(
 //            height: 30,
 //          ),
@@ -24,6 +38,14 @@ class SponsorPage extends StatelessWidget {
       ),
       title: "Sponsors",
     );
+  }
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
 
