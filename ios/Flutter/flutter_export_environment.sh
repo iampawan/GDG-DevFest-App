018296c321351f09df7aa62246780d470aff5dcd
Brian Salvattore <briansalvattore@gmail.com>
2019-12-07 16:12:48 -0300

    update dependencies

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mariovillelalarraza/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mariovillelalarraza/sandBox/PROYECTOS/devFest/GDG-DevFest-App"
-export "FLUTTER_TARGET=/Users/mariovillelalarraza/sandBox/PROYECTOS/devFest/GDG-DevFest-App/lib/main.dart"
+export "FLUTTER_ROOT=D:\Workspace\Flutter\SDK"
+export "FLUTTER_APPLICATION_PATH=D:\Workspace\Flutter\DevFest\GDG-DevFest-App"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/mariovillelalarraza/development/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Workspace\Flutter\SDK\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.4"
 export "FLUTTER_BUILD_NUMBER=5"
-export "TRACK_WIDGET_CREATION=true"
