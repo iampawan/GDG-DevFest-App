51d8156dc2fe58ac898c33f80ddbafe0a36c2a64
Kelebra <ricardokelebra@gmail.com>
2019-12-09 13:43:00 -0600

    cambios minimos ois

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mariovillelalarraza/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mariovillelalarraza/sandBox/PROYECTOS/devFest/GDG-DevFest-App"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/kalebra/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kalebra/Documents/devFest/GDG-DevFest-App"
+export "FLUTTER_TARGET=/Users/kalebra/Documents/devFest/GDG-DevFest-App/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/mariovillelalarraza/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kalebra/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.4"
 export "FLUTTER_BUILD_NUMBER=5"
